15a7d3a7cb81c0f6c08c6a475a3f9f7912c1236f
Nataliia <nataliiaqa8@gmail.com>
2024-09-08 18:43:32 +0200

    asdd

--- src/aos.js
+++ src/aos.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 import { Splide } from '@splidejs/splide';
 import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
@@ -9,3 +11,31 @@ const splide = new Splide('.splide', {
     pauseOnHover:false,
   },
 }).mount({ AutoScroll });
+
+
+
+
+
+
+
+
+// script.js
+// Отримати кнопку
+const scrollToTopBtn = document.querySelector('.scrollToTopBtn');
+
+// Показувати/сховувати кнопку в залежності від прокрутки
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = 'block'; // Показати кнопку
+  } else {
+    scrollToTopBtn.style.display = 'none'; // Сховати кнопку
+  }
+};
+
+// Прокрутка до верху при натисканні на кнопку
+scrollToTopBtn.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior:'smooth' // Плавна прокрутка
+  });
+};
