f55e9e4092ead5f5f2cb366da607b57f7012f176
Nataliia <nataliiaqa8@gmail.com>
2024-09-08 17:28:58 +0200

    sdgsdgsd

--- src/aos.js
+++ src/aos.js
@@ -1,3 +1,11 @@
-import './js/mobile-menu';
+import { Splide } from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
-  AOS.init();
+const splide = new Splide('.splide', {
+  type: 'loop',
+  autoScroll : {
+    speed : 2,
+    rewind: true,
+    pauseOnHover:false,
+  },
+}).mount({ AutoScroll });
